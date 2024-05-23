09a1377d61c6d189679f322c20d86b07c435ff49
Nii Derry <derricktagoe@gmail.com>
2024-05-23 13:06:28 +0000

    fetch api

---
+++ src/components/search.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+
+const URL = "https://api.spoonacular.com/recipes/complexSearch";
+const API_KEY = "68dca29813b2460a8a9cd05f37ba6232";
+
+export default function Search({ foodData, setFoodData }) {
+  const [query, setQuery] = useState("pizza");
+
+  // syntax of a use effect hook
+  useEffect(() => {
+    async function fetchFood() {
+      const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
+      const data = await res.json();
+      console.log(data.results);
+      setFoodData(data.results);
+    }
+    fetchFood();
+  }, [query]);
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+    </div>
+  );
+}
